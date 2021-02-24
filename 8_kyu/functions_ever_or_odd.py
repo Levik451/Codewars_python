583ae599813bad1c1e61158ce366ed02412864d6
Igor <yalevik@yandex.ru>
2021-02-24 23:33:12 +0300

    Initial commit

---
+++ 8_kyu/functions_ever_or_odd.py
@@ -0,0 +1,25 @@
+"""
+
+"""
+# Мое стандартное решение
+def even_or_odd(number):
+  if number % 2 == 0:
+    return "Even"
+  else:
+    return "Odd"
+print(even_or_odd(12))
+
+# Красивое решение
+# def even_or_odd(number):
+#   return ["Even", "Odd"][number % 2]
+
+# Более красивое решение
+# def even_or_odd(number):
+#     return 'Odd' if number % 2 else 'Even'
+# print(even_or_odd(12))
+
+# Решение №1 через lambda функцию
+# even_or_odd = lambda number: "Odd" if number % 2 else "Even"
+
+# Решение №2 через lambda функцию
+# even_or_odd = lambda n: ["Even","Odd"][n % 2]
